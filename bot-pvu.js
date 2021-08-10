ade2f95aba05cb6423d845f115ee2eb9ae67ce56
fakhripraya <fakhri@indomaret.co.id>
2021-08-10 15:05:10 +0700

    Update bot-pvu.js
    - fix error karna lag

--- bot-pvu.js
+++ bot-pvu.js
@@ -5,55 +5,42 @@ if (typeof (maxPage) !== 'undefined') {
     maxPage = maxPage[0];
 }
 
-var curPage = document.getElementsByClassName("currentPage tw-mr-2")[0];
-
-if (typeof (curPage) !== 'undefined')
-    curPage = curPage.innerText;
-else
-    curPage = 1;
-
 var pagePass = 0;
-console.log("Loading...")
+console.log("Loading")
 
-for (let page = curPage; page <= maxPage; page++) {
+var interval = setInterval(() => {
+    var loadingGif = document.getElementsByClassName("loading-page");
+    if (loadingGif.length === 0) {
+        var curPage = document.getElementsByClassName("currentPage tw-mr-2")[0];
 
-    console.log("Page ke: " + page);
+        if (typeof (curPage) !== 'undefined')
+            curPage = curPage.innerText;
 
-    //detect loading gif
-    var loading = true;
-    while (loading) {
-        var loadingGif = document.getElementsByClassName("loading-page");
-        console.log(loadingGif.length)
-        if (loadingGif.length === 0) {
-            loading = false;
-        }
-    }
-
-    var validCount = 0;
-    var waterParent = document.getElementsByClassName("tw-absolute tool-icon");
-    for (let i = 0; i < waterParent.length; i++) {
-        if (waterParent[i].src === "https://marketplace.plantvsundead.com/_nuxt/img/water@3x.d5ca50d.png") {
-            console.log(waterParent[i].parentElement.children[2].innerText)
-            if (waterParent[i].parentElement.children[2].innerText < 40) {
-                validCount++
+        var validCount = 0;
+        var waterParent = document.getElementsByClassName("tw-absolute tool-icon");
+        for (let i = 0; i < waterParent.length; i++) {
+            if (waterParent[i].src === "https://marketplace.plantvsundead.com/_nuxt/img/water@3x.d5ca50d.png") {
+                console.log(waterParent[i].parentElement.children[2].innerText)
+                if (waterParent[i].parentElement.children[2].innerText < 40) {
+                    validCount++
+                }
             }
         }
-    }
 
-    console.log("Page: " + (typeof (curPage) === 'undefined' ? 1 : curPage))
-    console.log("of: " + (typeof (maxPage) === 'undefined' ? 1 : maxPage))
-    if (curPage == maxPage) {
-        console.log("dah page terakhir")
-        clearInterval(interval);
-    } else if (pagePass === 0) {
-        console.log("Initial page akan selalu dilewati (asumsi sudah dicek sebelumnya)")
-        pagePass++
-        document.querySelectorAll('.tw-mt-6')[1].children[4].click();
-    } else if (validCount === 0) {
-        document.querySelectorAll('.tw-mt-6')[1].children[4].click();
-    } else {
-        console.log("Ada yang kering nih")
-        clearInterval(interval);
+        console.log("Current Page: " + (typeof (curPage) === 'undefined' ? 1 : curPage))
+        console.log("Total Page: " + (typeof (maxPage) === 'undefined' ? 1 : maxPage))
+        if (curPage == maxPage) {
+            console.log("Sudah page terakhir")
+            clearInterval(interval);
+        } else if (pagePass === 0) {
+            console.log("Page pertama selalu dilewati")
+            pagePass++
+            document.querySelectorAll('.tw-mt-6')[1].children[4].click();
+        } else if (validCount === 0) {
+            document.querySelectorAll('.tw-mt-6')[1].children[4].click();
+        } else {
+            console.log("Ada yang kering nih")
+            clearInterval(interval);
+        }
     }
-
-}
+}, 2000);
