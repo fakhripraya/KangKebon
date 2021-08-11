2357c2416f9847552b2d7557f919eea2ab4df719
fakhripraya <fakhri@indomaret.co.id>
2021-08-11 15:56:28 +0700

    update
    - fix the initial page problem, by specificaly target the plot element class name

--- monyet-pengganggu-pvu-gagak.user.js
+++ monyet-pengganggu-pvu-gagak.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kang Gagak
 // @namespace    http://tampermonkey.net/
-// @version      1.0.4
+// @version      1.0.7
 // @description  try to take over the crow!
 // @author       You
 // @match        https://marketplace.plantvsundead.com/farm/other/*
@@ -48,47 +48,48 @@
     console.log("Loading...")
     var interval = setInterval(() => {
         var loadingGif = document.getElementsByClassName("loading-page");
-
-        var bodyElement = document.getElementById("__layout").children[0].children[1];
+        var bodyElement = document.getElementById("__layout").children[0].children[1].children[0];
 
         if (typeof (bodyElement) !== 'undefined') {
-            if (loadingGif.length === 0) {
+            if (bodyElement.className === "content-wrapper tw-bg-farm-mobile sm:tw-bg-farm-desktop tw-p-2") {
+                if (loadingGif.length === 0) {
 
-                var curPage = document.getElementsByClassName("currentPage tw-mr-2")[0];
-                if (typeof (curPage) !== 'undefined')
-                    curPage = curPage.innerText;
+                    var curPage = document.getElementsByClassName("currentPage tw-mr-2")[0];
+                    if (typeof (curPage) !== 'undefined')
+                        curPage = curPage.innerText;
 
-                var maxPage = document.getElementsByClassName("text tw-mr-2")[1];
-                if (typeof (maxPage) !== 'undefined') {
-                    maxPage = maxPage.innerText.match(/\d+/g);
-                    maxPage = maxPage[0];
-                }
-
-                var validCount = 0;
-                let a = document.querySelectorAll('.crow-icon')
-                for (let index = 0; index < a.length; index++) {
-                    let b = a[index];
-                    if (b.getAttribute('style') == "") {
-                        validCount += 1;
+                    var maxPage = document.getElementsByClassName("text tw-mr-2")[1];
+                    if (typeof (maxPage) !== 'undefined') {
+                        maxPage = maxPage.innerText.match(/\d+/g);
+                        maxPage = maxPage[0];
                     }
-                }
 
-                console.log("Current Page: " + (typeof (curPage) === 'undefined' ? 1 : curPage))
-                console.log("Total Page: " + (typeof (maxPage) === 'undefined' ? 1 : maxPage))
-                if (curPage == maxPage) {
-                    if (validCount > 0) {
-                        dryWaterToast.showToast();
+                    var validCount = 0;
+                    let a = document.querySelectorAll('.crow-icon')
+                    for (let index = 0; index < a.length; index++) {
+                        let b = a[index];
+                        if (b.getAttribute('style') == "") {
+                            validCount += 1;
+                        }
+                    }
+
+                    console.log("Current Page: " + (typeof (curPage) === 'undefined' ? 1 : curPage))
+                    console.log("Total Page: " + (typeof (maxPage) === 'undefined' ? 1 : maxPage))
+                    if (curPage == maxPage) {
+                        if (validCount > 0) {
+                            dryWaterToast.showToast();
+                            console.log("Ada crow!");
+                        }
+
+                        lastPageToast.showToast();
+                        console.log("Sudah page terakhir");
+                        clearInterval(interval);
+                    } else if (validCount === 0) {
+                        document.querySelectorAll('.tw-mt-6')[1].children[4].click();
+                    } else {
+                        crowToast.showToast();
                         console.log("Ada crow!");
                     }
-
-                    lastPageToast.showToast();
-                    console.log("Sudah page terakhir");
-                    clearInterval(interval);
-                } else if (validCount === 0) {
-                    document.querySelectorAll('.tw-mt-6')[1].children[4].click();
-                } else {
-                    crowToast.showToast();
-                    console.log("Ada crow!");
                 }
             }
         }
