ba840b3ca5e805b42bdd3fe69907900271e531d5
alteregogi <>
2021-08-11 14:13:52 +0700

    update version

--- monyet-pengganggu-pvu.user.js
+++ monyet-pengganggu-pvu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kang Kebun
 // @namespace    http://tampermonkey.net/
-// @version      1.0.5
+// @version      1.0.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://marketplace.plantvsundead.com/farm/other/*
